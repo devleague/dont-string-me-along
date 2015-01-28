73db733bc01a8144fb7b2f4f865bd2b33872ac86
Teresa Nededog <tnededog@my.hpu.edu>
2015-01-27 19:07:40 -1000

    Setup tests

---
+++ app.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  findMissingNumbers : function () {
+
+
+  },
+
+  swapWords : function () {
+
+
+  },
+
+  twoStrings : function () {
+
+
+  },
+
+  palindrome : function () {
+
+
+  }
+};
