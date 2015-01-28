117200e223851ec4ea74ce44f49fbe10e4df7deb
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-28 11:29:18 -1000

    adds swapWords function and implements IIFE

--- dontstringmealong.js
+++ dontstringmealong.js
@@ -1,13 +1,32 @@
-var numSequence = "9899100101103104105";
-var missingNums = [];
+(function () {
+  var str = "9899100101103104105";
 
-function missingNumPush (){
-  for (var i = 0; i < 10; i++){
-    if (str.indexOf(i) === -1){
-      missingNums.push(i);
+  function missingNumPush (){
+  var missingNums = [];
+    for (var i = 0; i < 10; i++){
+      if (str.indexOf(i) === -1){
+        missingNums.push(i);
+      }
     }
+  return missingNums;
   }
-}
+  missingNumPush();
+})();
 
-change
+(function () {
+  // console.log("test");
+  var sample = "Jay Won";
+  var whiteSpace = sample.indexOf(" ");
+
+  function swapWords (){
+    var secondWord = sample.slice(0,whiteSpace);
+    var firstWord = sample.slice(whiteSpace, sample.length);
+    var swapped = (firstWord + ", " + secondWord);
+    return swapped;
+    //return(firstWord + ", " + secondWord);
+  }
+
+  swapWords();
+})();
+
 
